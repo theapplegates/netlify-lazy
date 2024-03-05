e508a15512e96a7f981695ec5727bdb35610220c
Paul <paul@theapplegates.com>
2024-03-04 20:03:13 -0500

    test

--- src/utils/picture.js
+++ src/utils/picture.js
@@ -1,11 +1,21 @@
-// Generate an picture tag with image src URLs which use Neltify image transforms
-
 module.exports = (url, alt = "Missing alt text") => {
   return `<picture>
-  <source srcset="/images/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
-  <source srcset="/images/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
-  <img src="/images/${url}?nf_resize=fit&w=500" alt="${alt}" />
-</picture>`;
-};
+    <source srcset="/images/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
+    <source srcset="/images/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
+    <source srcset="/images/${url}?nf_resize=fit&w=500" media="(min-width: 500px)"> <img src="/images/${url}?nf_resize=fit&w=300" alt="${alt}" /> </picture>`;
+};   
+
+
+// old code below
+
+// Generate an picture tag with image src URLs which use Neltify image transforms
+
+// module.exports = (url, alt = "Missing alt text") => {
+  // return `<picture>
+  // <source srcset="/images/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
+  // <source srcset="/images/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
+  // <img src="/images/${url}?nf_resize=fit&w=500" alt="${alt}" />
+// </picture>`;
+// };
 
 
