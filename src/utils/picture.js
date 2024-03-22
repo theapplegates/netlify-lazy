1f4eb3ba9e922d3411a233bac86a0bade555e53c
Paul <paul@theapplegates.com>
2024-03-22 14:10:31 -0400

    Update picture.js

--- src/utils/picture.js
+++ src/utils/picture.js
@@ -1,21 +1,29 @@
+const sharp = require('sharp');
+
 module.exports = (url, alt = "Missing alt text") => {
-  return `<picture>
-    <source srcset="/images/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
-    <source srcset="/images/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
-    <source srcset="/images/${url}?nf_resize=fit&w=500" media="(min-width: 500px)"> <img src="/images/${url}?nf_resize=fit&w=300" alt="${alt}" /> </picture>`;
-};   
+    const imageName = url.split('.').shift(); // Extract image name (assuming no dots in the name itself)
+    const outputFormats = ['avif', 'webp', 'jpg'];
 
+    // Generate all required image formats 
+    const formatPromises = outputFormats.map(format => { 
+        return sharp(`/images/${url}`) // Adjust the path if needed
+            .resize({ width: /* Adjust based on your media queries */ }) 
+            [format]({ quality: /* Your desired quality setting */ }) 
+            .toFile(`/images/${imageName}.${format}`); 
+    });
 
-// old code below
-
-// Generate an picture tag with image src URLs which use Neltify image transforms
-
-// module.exports = (url, alt = "Missing alt text") => {
-  // return `<picture>
-  // <source srcset="/images/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
-  // <source srcset="/images/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
-  // <img src="/images/${url}?nf_resize=fit&w=500" alt="${alt}" />
-// </picture>`;
-// };
-
-
+    // Wait for all formats to be generated
+    Promise.all(formatPromises)
+        .then(() => {
+            return `<picture>
+                <source srcset="/images/${imageName}.avif" type="image/avif" media="(min-width: 1200px)">
+                <source srcset="/images/${imageName}.webp" type="image/webp" media="(min-width: 1200px)">
+                <source srcset="/images/${imageName}.avif" type="image/avif" media="(min-width: 740px)">
+                <source srcset="/images/${imageName}.webp" type="image/webp" media="(min-width: 740px)">
+                <source srcset="/images/${imageName}.avif" type="image/avif" media="(min-width: 500px)">
+                <source srcset="/images/${imageName}.webp" type="image/webp" media="(min-width: 500px)">
+                <img src="/images/${imageName}.jpg" alt="${alt}" />
+            </picture>`;
+        })
+        .catch(err => console.error("Error generating image formats:", err)); 
+};
