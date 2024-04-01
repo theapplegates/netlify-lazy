6a995c981fc0af5873e6eaef26d6123491d1dc7d
Paul <paul@theapplegates.com>
2024-04-01 14:01:55 -0400

    first commit

--- src/utils/lazy-picture.js
+++ src/utils/lazy-picture.js
@@ -3,8 +3,8 @@
 
 module.exports = (url, alt = "Missing alt text") => {
   return `<picture class="lazy lazy-initial">
-  <source srcset="/images/tiny/${url}" media="(min-width: 1200px)">
-  <source srcset="/images/tiny/${url}" media="(min-width: 740px)">
+  <source srcset="/images/tiny/${url}" media="(min-width: 1600px)">
+  <source srcset="/images/tiny/${url}" media="(min-width: 480px)">
   <img src="/images/tiny/${url}" alt="${alt}" /></picture>`;
 };
 
